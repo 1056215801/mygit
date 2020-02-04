df23fa94458863a2a23f8b706a9b83abb6c31adb
1056215801 <46217527+1056215801@users.noreply.github.com>
2020-02-04 16:27:31 +0800

    学生类修改

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,4 +1,9 @@
 package com.example.demo.controller;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 public class StudentController {
+    public static void main(String[] args) {
+        System.out.println("添加新代码");
+    }
 }
